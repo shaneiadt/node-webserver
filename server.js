c49ee4515964d9d42c69efa06c97cac191ce618c
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-01-07 12:30:48 +0000

    Add projects page

--- server.js
+++ server.js
@@ -50,6 +50,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        title: 'Projects Page'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to fulfill this request.'
